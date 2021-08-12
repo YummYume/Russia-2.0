9b7c53790506f7f99e99cdcdb5eca48072bde229
YamYam <givsoul@gmail.com>
2021-08-12 10:05:37 +0200

    Purge

--- scripts/russia_reworked.py
+++ scripts/russia_reworked.py
@@ -10,9 +10,17 @@ if __name__ == '__main__':
 
         f = open("save.txt", "w")
 
-        for filename in os.listdir(directory):
-            tag = filename.split()
-            f.write("add_to_array = { global.russia_reworked_countries_load = %s }\n" % tag[0])
+        #for filename in os.listdir(directory):
+        #    tag = filename.split()
+        #    f.write("add_to_array = { global.russia_reworked_countries_load = %s }\n" % tag[0])
+
+        currentid = 557
+        while currentid <= 591:
+            f.write(" soviets.%s.t:0 \"The Great Purge - \"\n" % currentid)
+            f.write(" soviets.%s.d:0 \"\"\n" % currentid)
+            f.write(" soviets.%s.a:0 \"He has to go.\"\n" % currentid)
+            f.write(" soviets.%s.b:0 \"He is not guilty.\"\n" % currentid)
+            currentid += 1
 
         f.close
 
