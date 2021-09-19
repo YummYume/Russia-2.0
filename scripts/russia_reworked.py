b1e418e2a4a308dcd51ef497feb0c2b84ddb8862
YamYam <givsoul@gmail.com>
2021-09-19 22:29:39 +0200

    Anarchist Ukraine

--- scripts/russia_reworked.py
+++ scripts/russia_reworked.py
@@ -1,6 +1,29 @@
 # Created by Yam for the Russia Reworked mod. Free to use.
 
 import os
+import json
+import statistics
+
+def addSpace(n, numSpace = 3):
+    n = str(n).split('.')
+    n[0] = list(str(n[0]))
+    charCount = 0
+    r = ''
+
+    for c in reversed(n[0]):
+        r += c
+        charCount += 1
+
+        if charCount == numSpace:
+            r += ' '
+            charCount = 0
+
+    r = r[::-1]
+
+    if len(n) > 1:
+        r += '.%s' % n[1]
+
+    return r
 
 if __name__ == '__main__':
 
@@ -26,5 +49,21 @@ if __name__ == '__main__':
 
         print("Success")
 
-    except:
+        manpower = []
+
+        with open('states_2021-07-31.json', encoding='utf-8-sig') as jsonFile:
+            states = json.load(jsonFile)
+
+            for key, state in states.items():
+                if isinstance(state, dict) and "manpower" in state:
+                    manpower.append(int(state["manpower"]))
+
+        totalManpower = addSpace(sum(manpower))
+        averageManpower = addSpace(statistics.mean(manpower))
+
+        print("Total manpower : %s" % totalManpower)
+        print("Average manpower : %s" % averageManpower)
+
+    except Exception as e:
         print("Something went wrong")
+        print(e)
