b968264eee131fd157fe24a4bcb84ad070bceeed
YamYam <givsoul@gmail.com>
2021-11-07 21:48:07 +0100

    Achievements

---
+++ scripts/achievement_generator_tt.py
@@ -0,0 +1,34 @@
+# Created by Yam for the Russia Reworked mod. Free to use.
+import traceback
+
+if __name__ == "__main__":
+
+    try:
+        achievements = [
+            "cleaning_red_carpet",
+            "revolution_completed",
+            "speedrun_urals",
+            "not_sus",
+            "new_world_order",
+            "red_army",
+            "world_without_borders",
+            "rush_b",
+            "fourth_rome",
+            "gotcha",
+            "drunkard_wheel",
+            "copyright",
+            "running_90",
+            "quick_revolution",
+            "great_deeds",
+            "rome_tsarigrad_moscow",
+            "1905_avenged",
+            "lacplesis",
+            "third_friend",
+            "national_priority",
+            "click_me",
+            "big_tower"
+        ]
+
+    except Exception:
+        print("Something went wrong...")
+        print(traceback.format_exc())
